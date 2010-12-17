b7effceccbda2d9f98bc9475ad849b9b2080a56a
Florian <AlwaysCarryABible@googlemail.com>
2010-12-17 10:39:55 +0100

    Changed Routes and removed default Rails page

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  describe "GET /" do
+    it "should have the index at '/'" do
+      get '/'
+      response.should have_selector('title', :content => 'Home')
+    end
+  end
+  describe "GET /contact" do
+    it "should have the contact page at '/contact'" do
+      get '/contact'
+      response.should have_selector('title', :content => 'Contact')
+    end
+  end
+  describe "GET /about" do
+    it "should have the about page at '/about'" do
+      get '/about'
+      response.should have_selector('title', :content => 'About')
+    end
+  end
+  describe "GET /help" do
+    it "should have the help page at '/help'" do
+      get '/help'
+      response.should have_selector('title', :content => 'Help')
+    end
+  end
+end
